86b12c4f5f1416b6057761bfd371d4a8ebcd46fe
Vedant Chainani <vedantchainani1084@gmail.com>
2025-04-05 07:12:33 +0530

    chore: add dummy email data

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,5 +5,5 @@ EXAMPLES_DIR="examples"
 for dir in "$EXAMPLES_DIR"/*/; do
   folder_name=$(basename "$dir")
   echo "Compiling package: $folder_name"
-  nargo compile --package="$folder_name"
+  nargo compile --package="$folder_name" --silence-warnings
 done
